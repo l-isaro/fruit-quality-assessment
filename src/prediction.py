2ec541ff3c2a43f6ffca00a47216151e7632f2a3
l-isaro <l.isaro@alustudent.com>
2025-08-03 21:32:48 +0200

    changing path back

--- src/prediction.py
+++ src/prediction.py
@@ -15,7 +15,7 @@ def load_model():
     current_dir = os.getcwd()
     st.write(f"📁 Current working directory: {current_dir}")
     st.write(os.listdir("/opt/render/project/"))
-    return tf.keras.models.load_model("../../models/fruit_model.keras")
+    return tf.keras.models.load_model("../models/fruit_model.keras")
 
 model = load_model()
 class_labels = [
